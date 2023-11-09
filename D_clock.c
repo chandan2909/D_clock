600088b88b14c3c7e4dac70d86d07b0cf8f50adc
chandan2909 <vijaypathakssa3@gmail.com>
2023-11-09 12:48:20 +0530

    Initial commit

---
+++ D_clock.c
@@ -0,0 +1,37 @@
+
+#include<stdio.h>
+int main()
+{
+ int h,m,s;
+ int d = 1000;
+ printf("Set time : \n");
+ scanf("%d %d %d",&h,&m,&s);
+ if(h>12 || m>60 || s>60)
+ {
+   printf("Error ! please try again");
+   exit(0);1
+
+ }
+ while(1)
+ {
+   s++;
+   if(s>59)
+   {
+      m++;
+      s=0;
+   }
+   if(m>59)
+   {
+      h++;
+      m=0;
+   }
+   if(h>12)
+   {
+      h=1;
+   }
+   printf("\n Clock : ");
+   printf("\n %02d: %02d: %02d: ",h,m,s);
+   Sleep(d);
+   system("cls");
+ }
+}
